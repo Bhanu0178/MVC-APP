ed6c420c6130888dbbc9b71290eac3ead6bfe8e0
Bhanu1132 <kb7097@gmail.com>
2024-12-04 19:33:17 +0530

    v1

--- 0001-Welcome-SpringBoot-MVC/src/main/java/com/ease/controller/WelcomeController.java
+++ 0001-Welcome-SpringBoot-MVC/src/main/java/com/ease/controller/WelcomeController.java
@@ -1,13 +1,13 @@
 package com.ease.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
 	
-	@GetMapping("/show")
+	@RequestMapping("/show")
 	public ModelAndView getWelcomeMsg() {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.addObject("msg", "Welcome To Spring MVC".toUpperCase());
